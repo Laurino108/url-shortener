d3bc996ea0095b6d35ff561614911fefe3d33c33
Laurin Stapf <stapf_laurin@outlook.de>
2023-12-30 01:22:32 +0100

    Fixed some small errors with uggly code :/

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,14 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
